0d5daf0ac5dfb19af28088565efa181c3d96f206
Jennifer Nnadi <jennify.nnadi@gmail.com>
2024-09-01 19:23:22 -0400

    Routes set up for client

---
+++ client/src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <a herf='/auth/spotify'> Login with Spotify </a>
+    </div>
+  )
+}
+
+export default Login;
---
+++ client/src/components/Playlists.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Playlists = () => {
+  return (
+    <div>
+      <h1> Your Playlists</h1>
+      <p> here are all the playlists you have created or contributed to</p>
+    </div>
+  );
+};
+
+export default Playlists;
